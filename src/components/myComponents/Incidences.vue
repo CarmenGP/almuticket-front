b060c3ffa1b712786c5127e6750ab543be574e03
LolaGM <canal030w@gmail.com>
2023-03-10 14:49:58 +0100

    api data on table

--- src/components/myComponents/Incidences.vue
+++ src/components/myComponents/Incidences.vue
@@ -1,21 +1,31 @@
 <script>
 import axios from 'axios';
-export default {
-    data() {
-        return {
-            incidencesData :null,
-        }
-    },
-    created() {             
-		axios.get('https://jsonplaceholder.typicode.com/users/1').then((result)=> {this.incidencesData = result.data
-                    })
-                },
+export default{
+	data() {
+		return {
+			users: []
+		}
+	},
+	created() {
+  	axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      this.users = response.data.map(user => ({
+        name: user.name ,
+		ticket: user.username,
+        email: user.email,
+		phone: user.phone,
+		site: user.website,
+		company: user.company.name,
+		address:user.address.street
+    }))
+    })
+	}
 }
 </script>
 <template>
     <div>     
 		<!--Card-->
-		<div v-if="incidencesData" id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+		<div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
 			<table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
 				<thead>
 					<tr>
@@ -26,42 +36,30 @@ export default {
 						<th data-priority="5">Programa</th>
 						<th data-priority="6">Proyecto</th>
 						<th data-priority="7">Descripción</th>
-						<th data-priority="5">Sede</th>
-						<th data-priority="6">Estado</th>
+						<th data-priority="8">Sede</th>
+						<th data-priority="9">Estado</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>{{ incidencesData.name  }}</td>
-						<td>{{ incidencesData.name  }}</td>
-						<td></td>
-						<td>61</td>
-						<td>2011/04/25</td>
-						<td>$320,800</td>
-						<td>61</td>
-						<td>2011/04/25</td>
-						<td>$320,800</td>
+					<tr v-for="user in users" :key="user.id">
+						<td>2023/03/01</td>
+						<td>{{ user.name  }}</td>
+						<td>{{ user.ticket  }}</td>
+						<td>{{ user.name  }}</td>
+						<td>{{ user.email  }}</td>
+						<td>{{ user.company  }}</td>
+						<td>{{ user.name  }}</td>
+						<td>{{ user.address  }}</td>
+						<td>{{ user.name  }}</td>
 					</tr>
-
 					<!-- Rest of your data (refer to https://datatables.net/examples/server_side/ for server side processing)-->
-
-					<tr>
-						<td>Donna Snider</td>
-						<td>Customer Support</td>
-						<td>New York</td>
-						<td>27</td>
-						<td>2011/01/25</td>
-						<td>$112,000</td>
-						<td>61</td>
-						<td>2011/04/25</td>
-						<td>$320,800</td>
-					</tr>
 				</tbody>
 			</table>
 		</div>
 		<!--/Card-->
     </div>
 </template>
+
 <style lang="css" scoped>
 		/*Overrides for Tailwind CSS */
 
