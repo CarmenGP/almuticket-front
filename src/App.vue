bdce74d427eee82871ebd845ceab1199a3e00f54
LolaGM <canal030w@gmail.com>
2023-03-10 17:32:31 +0100

    favicon and title added. Also deleted VUE logo and text

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Inicio</RouterLink>
+        <RouterLink to="/about">Sobre AlmuTicket</RouterLink>
         <RouterLink to="/incidences">Incidencias</RouterLink>
       </nav>
     </div>
@@ -64,9 +59,6 @@ nav a:first-of-type {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
   header .wrapper {
     display: flex;
