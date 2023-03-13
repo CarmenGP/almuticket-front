4e3f53d47656453d322a6981d63db281937d36fc
LolaGM <canal030w@gmail.com>
2023-03-13 12:58:19 +0100

    register style in progress

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  
+
   <header>
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Inicio</RouterLink>
         <RouterLink to="/about">Sobre AlmuTicket</RouterLink>
         <RouterLink to="/incidences">Incidencias</RouterLink>
+        <RouterLink to="/register">Registro</RouterLink>
+
       </nav>
     </div>
   </header>
---
+++ src/components/myComponents/Register.vue
@@ -0,0 +1,69 @@
+<script>
+export default{}
+</script>
+
+<template>
+    <div class="container mx-auto p-4 bg-white">
+        <div class="w-full md:w-1/2 lg:w-1/3 mx-auto my-12">
+            <h1 class="text-lg font-bold">Registro en Asociación Arrabal</h1>
+            <form class="flex flex-col mt-4">
+            <label>Nombre
+                <input
+                type="text"
+                name="full-name"
+                class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                placeholder="Nombre"
+                />
+            </label>
+            <label>Apellidos
+                <input
+                type="text"
+                name="full-name"
+                class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                placeholder="Nombre"
+                />
+            </label>
+            <label>Correo
+            <input
+                type="email"
+                name="email"
+                class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                placeholder="Correo electrónico"
+            />
+            </label>
+            <label>Contraseña
+            <input
+                type="password"
+                name="password"
+                class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                placeholder="Contraseña"
+            />
+            </label>
+            <label>Confirmar contraseña
+            <input
+                type="password"
+                name="repeat-password"
+                class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                placeholder="Confirmar constraseña"
+            />
+            </label>
+            <button
+                type="submit"
+                class="mt-4 px-4 py-3  leading-6 text-base rounded-md border border-transparent text-white focus:outline-none bg-blue-500 text-blue-100 hover:text-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer inline-flex items-center w-full justify-center items-center font-medium focus:outline-none"
+            >
+                Registrarse
+            </button>            
+            </form>
+        </div>
+    </div>
+</template>
+
+<style language="CSS" scoped>
+form{
+    font-family: 'Nunito Sans', sans-serif;
+}
+label{
+    color:#F08419;
+}
+
+</style>
