5dd5cb155a5431b4acc1dd0fb1178b34b708174b
raquel2002x <palomofuentesraquel2002@gmail.com>
2023-03-13 18:37:36 +0100

    header wrong position

---
+++ src/components/myComponents/Header.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name:'Header'
+}
+</script>
+
+
+<template>
+    <nav class="navbar">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Crear Reserva</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Crear Incidencia</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Mis Tickets</a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+</template>
+
+<style>
+.navbar{
+    display:flex;
+    background-color:#F08419;
+    margin-top: 0;
+    align-items: flex-start;
+}
+</style>
