0a1a6b33a28adaa3f96724c8bd387eb7dc93fe22
FlorTiscornia <flora.tiscornia@gmail.com>
2023-03-17 13:05:05 +0100

    style fixed

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import FooterVue from './components/myComponents/FooterVue.vue';
+import Header from './components/myComponents/Header.vue'
+
 </script>
-
 <template>
-  <!-- <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Inicio</RouterLink>
-        <RouterLink to="/about">Sobre AlmuTicket</RouterLink>
-        <RouterLink to="/incidences">Incidencias</RouterLink>
-      </nav>
-      
-    </div>
-  </header> -->
-  
+  <Header/>
   <RouterView />
-
   <FooterVue />
 </template>
-
 <style scoped>
-import Header from './components/myComponents/Header.vue'
-</script>
-
-<template>
-<Header/>
-</template>
-
-<style>
-
 </style>
