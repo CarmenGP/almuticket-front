27fa2aacc80dc2ae90a407eedf760105ab35659b
pagudelo884 <pagudelo884@gmail.com>
2023-03-17 14:01:29 +0100

    edit table

--- src/views/MyTickestView.vue
+++ src/views/MyTickestView.vue
@@ -6,15 +6,11 @@ components: { Tickest },
 </script>
 <template>
     <div>
-    <h1>Tickets</h1>
     <br>
     <Tickest/>
     </div>
 </template>
 <style>
-h1{
-    text-align: center;
-    border: 3px solid #F08419;
-}
+
 
 </style>
