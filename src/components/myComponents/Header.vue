66f41d6684aa2692e4c3923908a8917d2f28376e
raquel2002x <palomofuentesraquel2002@gmail.com>
2023-03-19 16:04:22 +0100

    add form with provisional fills

--- src/components/myComponents/Header.vue
+++ src/components/myComponents/Header.vue
@@ -1,75 +1,93 @@
 <script>
-
+import { ref } from 'vue';
 export default {
-    name:'Header'
+    setup() {
+        let showMenu = ref(false);
+        const toggleNav = () => (showMenu.value = !showMenu.value);
+        return { showMenu, toggleNav };
+    }
 }
-
 </script>
 
-
 <template>
-    <div class="headerContainer">
-        <div class="logoContainer">
-            <a class="logoArrabal" href="#">
-            <img src="public/logoArrabal.png" alt="Logo Arrabal">
-            </a>
+    <div class="headerContainer bg-indigo-600">
+        <nav class="container px-4  py-2 mx-auto md:flex md:justify-between md:items-center ">
+            <div class="logoArrabal flex items-center justify-between">
+                <router-link     to="/"    class="text-xl font-bold text-gray-100  md:text-2xl ">
+                    <a class="logoArrabal" href="../views/login.vue">
+                        <img class="logoArrabal" src="public/logoArrabal.png" alt="Logo Arrabal">
+                    </a>
+                </router-link>
+            <!-- Mobile menu button -->
+                <div @click="toggleNav" class="flex md:hidden">
+                    <button  type="button" class="text-gray-100  hover:text-xl  focus:outline-none focus:text-white-400">
+                        <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                            <path  fill-rule="evenodd"  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"                   ></path>
+                        </svg>
+                    </button>
+                </div>
             </div>
-        <div class="menu">
-            <RouterLink to="/"><a>Inicio</a></RouterLink>
-            <RouterLink to="/about"><a>Crear Reserva</a></RouterLink>
-            <RouterLink to="/login"><a>Crear Incidencia</a></RouterLink>
-            <RouterLink to="/tickets"><a>Mis Tickets</a></RouterLink>
-        </div>
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+            <ul :class="showMenu ? 'flex' : 'hidden'"  class="menu flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
+                <li><RouterLink to="/"><strong><a href="../views/login.vue">Inicio</a></strong></RouterLink></li>
+                <li class="text-gray-100 hover:text-indigo-400"><RouterLink to="/booking"><strong><a href="../views/bookingForm.vue">Crear reserva</a></strong></RouterLink></li>
+                <li class="text-gray-100 hover:text-indigo-400"><RouterLink to="/incidence"><strong><a href="../views/incidenceForm.vue">Crear incidencia</a></strong></RouterLink></li>
+                <li class="text-gray-100 hover:text-indigo-400"><RouterLink to="/allTickets"><strong><a href="../views/allTickets.vue">Mis Tickets</a></strong></RouterLink></li>
+            </ul>
+        </nav>
     </div>
-</template>
+</template> 
 
 <style lang="css" scoped>
-@import "bootstrap/dist/css/bootstrap.min.css";
 
 .headerContainer{
     background-color:#F08419; 
-    height: 20%;
-    width: 100%!important;
-    display:flex;
-    flex-direction: row;
-    justify-content: space-between;
-}  
-
-.logoContainer{
-    margin-top: 0px;
-    width: 20%;
-    align-items: center;
 }  
 
 .logoArrabal{
-    margin-top:10px;
-    margin-left: 10px;
-    align-items: center;
-
+    width: 75%;
+    margin-left: 2%;
 }
 
 .menu{
-    display: flex;
-    flex-direction: row;
-    margin-right: 0;
-    width: 50%;
-    align-items: center;
-    justify-content: space-between;
     margin-right: 10%;
 }
 a{
     text-decoration: none;
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
     color:white;
     margin:15px;
     justify-items: center;
-    align-items: baseline;
 }
 
 a:hover{
-    color:black;
+    color:white;
+    text-decoration: underline;
 }
+
+
+@media only screen and (max-width: 768px) {
+
+.logoArrabal{
+    width: 90%;
+    margin-left: 2%;
+}
+
+a{
+    text-decoration: none;
+    display: flex;
+    flex-direction: row;
+    color:white;
+    justify-items: center;
+}
+
+a:hover{
+    color:white;
+    text-decoration: underline;
+}
+}
+
+
 
 </style>
---
+++ src/components/myComponents/ReservationForm.vue
@@ -0,0 +1,126 @@
+<script>
+export default {
+    data() {
+        return {
+        form: {
+            estado: '',
+            programa: '',
+            proyecto: '',
+            sede: '',
+            sala: '',
+            fecha: '',
+            horaInicio: '',
+            horaFinal: '',
+            numeroAsistencias: '',
+            comentarios: ''
+        }
+        }
+    },
+    methods: {
+        submitForm() {
+        // Aquí podrías enviar el formulario a través de una petición HTTP o realizar alguna otra acción con los datos.
+        console.log(this.form);
+        }
+    }
+    }
+</script>
+
+<template>
+    <!-- <div class="container"> -->
+        <form @submit.prevent="submitForm" class="row g-3">
+        <div class="col-md-6">
+        <label for="estado" class="form-label">Estado:</label>
+        <select class="form-select" id="estado" v-model="form.estado">
+            <option value="">Seleccione un estado</option>
+            <option value="En proceso">Visto</option>
+            <option value="Terminado">Pendiente</option>
+            <option value="Terminado">En trámite</option>
+            <option value="Terminado">Finalizado</option>
+            <option value="Cancelado">Rechazado</option>
+        </select>
+        </div>
+        <div class="col-md-6">
+            <label for="programa" class="form-label">Programa:</label>
+            <input type="text" class="form-control" id="programa" v-model="form.programa">
+        </div>
+        <div class="col-md-6">
+            <label for="proyecto" class="form-label">Proyecto:</label>
+            <input type="text" class="form-control" id="proyecto" v-model="form.proyecto">
+        </div>
+        <div class="col-md-6">
+        <label for="sede" class="form-label">Sede:</label>
+        <select class="form-select" id="sede" v-model="form.sede">
+            <option value="">Seleccione un estado</option>
+            <option value="En proceso">Remoto</option>
+            <option value="Terminado">Edificio Galaxia</option>
+            <option value="Terminado">Innova. Social</option>
+            <option value="Terminado">Pasaje Begoña</option>
+            <option value="Cancelado">El Carmen</option>
+            <option value="Cancelado">Dos aceras</option>
+        </select>
+        </div>
+        <div class="col-md-6">
+            <label for="sala" class="form-label">Sala:</label>
+            <input type="text" class="form-control" id="sala" v-model="form.sala">
+        </div>
+        <div class="col-md-6">
+            <label for="fecha" class="form-label">Fecha:</label>
+            <input type="date" class="form-control" id="fecha" v-model="form.fecha">
+        </div>
+        <div class="col-md-6">
+            <label for="horaInicio" class="form-label">Hora de inicio:</label>
+            <input type="time" class="form-control" id="horaInicio" v-model="form.horaInicio">
+        </div>
+        <div class="col-md-6">
+            <label for="horaFinal" class="form-label">Hora final:</label>
+            <input type="time" class="form-control" id="horaFinal" v-model="form.horaFinal">
+        </div>
+        <div class="col-md-6">
+            <label for="asistencias" class="form-label">Número de asistencias:</label>
+            <input type="number" class="form-control" id="asistencias" v-model="form.numeroAsistencias">
+        </div>
+        <div class="col-md-12">
+            <label for="comentarios" class="form-label">Comentarios:</label>
+            <textarea class="form-control" id="comentarios" rows="3" v-model="form.comentarios"></textarea>
+        </div>
+        <div class="sendButton">
+            <button type="submit" class="btn ">Enviar</button>
+        </div>
+        </form>
+    <!-- g -->
+</template>
+
+<style lang="css" scoped>
+/* .container{
+    background-color:#FFF3D6;
+} */
+
+.g-3{
+    background-color:#FFF3D6;
+    margin: 10px 10px 10px 10px;
+    width: 100%;
+    height: 100%;
+}
+
+h1{
+    color:#F08419;
+    text-align:center;
+    margin-bottom: 5%;   
+}
+
+label{
+    color:#F08419;
+    font-weight: bold;
+}
+
+.sendButton{
+    font-weight: bold;
+    color: white;
+    background-color:#F08419;
+    border-radius:10px;
+    width:15%;
+    text-align: center;
+    margin-bottom: 10px;
+}
+
+</style>
