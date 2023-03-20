0af35b7c1979054fbf275639908bc59fb630819a
raquel2002x <palomofuentesraquel2002@gmail.com>
2023-03-20 09:57:42 +0100

    changed naming

--- src/components/myComponents/ReservationForm.vue
+++ src/components/myComponents/ReservationForm.vue
@@ -3,16 +3,16 @@ export default {
     data() {
         return {
         form: {
-            estado: '',
-            programa: '',
-            proyecto: '',
-            sede: '',
-            sala: '',
-            fecha: '',
-            horaInicio: '',
-            horaFinal: '',
-            numeroAsistencias: '',
-            comentarios: ''
+            state:'',
+            program:'',
+            proyect:'',
+            sede:'',
+            hall:'',
+            date:'',
+            startTime:'',
+            finishTime:'',
+            assistantsNumber:'',
+            comments:'',
         }
         }
     },
@@ -26,74 +26,76 @@ export default {
 </script>
 
 <template>
-    <!-- <div class="container"> -->
+    <div class="container"> 
         <form @submit.prevent="submitForm" class="row g-3">
         <div class="col-md-6">
-        <label for="estado" class="form-label">Estado:</label>
+        <label for="state" class="form-label">Estado:</label>
         <select class="form-select" id="estado" v-model="form.estado">
             <option value="">Seleccione un estado</option>
-            <option value="En proceso">Visto</option>
-            <option value="Terminado">Pendiente</option>
-            <option value="Terminado">En trámite</option>
-            <option value="Terminado">Finalizado</option>
-            <option value="Cancelado">Rechazado</option>
+            <option value="Visto">Visto</option>
+            <option value="Pendiente">Pendiente</option>
+            <option value="En trámite">En trámite</option>
+            <option value="Finalizado">Finalizado</option>
+            <option value="Rechazado">Rechazado</option>
         </select>
         </div>
         <div class="col-md-6">
-            <label for="programa" class="form-label">Programa:</label>
+            <label for="program" class="form-label">Programa:</label>
             <input type="text" class="form-control" id="programa" v-model="form.programa">
         </div>
         <div class="col-md-6">
-            <label for="proyecto" class="form-label">Proyecto:</label>
+            <label for="proyect" class="form-label">Proyecto:</label>
             <input type="text" class="form-control" id="proyecto" v-model="form.proyecto">
         </div>
         <div class="col-md-6">
         <label for="sede" class="form-label">Sede:</label>
         <select class="form-select" id="sede" v-model="form.sede">
             <option value="">Seleccione un estado</option>
-            <option value="En proceso">Remoto</option>
-            <option value="Terminado">Edificio Galaxia</option>
-            <option value="Terminado">Innova. Social</option>
-            <option value="Terminado">Pasaje Begoña</option>
-            <option value="Cancelado">El Carmen</option>
-            <option value="Cancelado">Dos aceras</option>
+            <option value="Remoto">Remoto</option>
+            <option value="Edificio Galaxia">Edificio Galaxia</option>
+            <option value="Innova. Social">Innova. Social</option>
+            <option value="Pasaje Begoña">Pasaje Begoña</option>
+            <option value="El Carmen">El Carmen</option>
+            <option value="Dos aceras">Dos aceras</option>
         </select>
         </div>
         <div class="col-md-6">
-            <label for="sala" class="form-label">Sala:</label>
+            <label for="hall" class="form-label">Sala:</label>
             <input type="text" class="form-control" id="sala" v-model="form.sala">
         </div>
         <div class="col-md-6">
-            <label for="fecha" class="form-label">Fecha:</label>
+            <label for="date" class="form-label">Fecha:</label>
             <input type="date" class="form-control" id="fecha" v-model="form.fecha">
         </div>
         <div class="col-md-6">
-            <label for="horaInicio" class="form-label">Hora de inicio:</label>
+            <label for="startTime" class="form-label">Hora de inicio:</label>
             <input type="time" class="form-control" id="horaInicio" v-model="form.horaInicio">
         </div>
         <div class="col-md-6">
-            <label for="horaFinal" class="form-label">Hora final:</label>
+            <label for="finishTime" class="form-label">Hora final:</label>
             <input type="time" class="form-control" id="horaFinal" v-model="form.horaFinal">
         </div>
         <div class="col-md-6">
-            <label for="asistencias" class="form-label">Número de asistencias:</label>
+            <label for="assistantsNumber" class="form-label">Número de asistencias:</label>
             <input type="number" class="form-control" id="asistencias" v-model="form.numeroAsistencias">
         </div>
         <div class="col-md-12">
-            <label for="comentarios" class="form-label">Comentarios:</label>
+            <label for="comments" class="form-label">Comentarios:</label>
             <textarea class="form-control" id="comentarios" rows="3" v-model="form.comentarios"></textarea>
         </div>
         <div class="sendButton">
-            <button type="submit" class="btn ">Enviar</button>
+            <button type="submit" class="btn "> Enviar </button>
         </div>
         </form>
-    <!-- g -->
+    </div>
 </template>
 
 <style lang="css" scoped>
-/* .container{
+.container{
     background-color:#FFF3D6;
-} */
+    height: 100%;
+    margin-top: 0px;
+}
 
 .g-3{
     background-color:#FFF3D6;
@@ -121,6 +123,11 @@ label{
     width:15%;
     text-align: center;
     margin-bottom: 10px;
+}
+
+.btn{
+    font-weight: bold;
+    color: white;
 }
 
 </style>
